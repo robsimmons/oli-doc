869c723597f5ef8c1c7d6a3ed1dcdce8224a52c8
Rob Simmons <rob@calculem.us>
2017-09-20 17:31:22 -0400

    Add a basic type signature

---
+++ src/question.ts
@@ -0,0 +1,44 @@
+/**
+ * A Question represents a single (multipart) OLI question.
+ *
+ * We build up questions imperatively, by adding one part at a time with the addInput function.
+ */
+export class Question {
+    private id: string;
+    private prompt: string;
+    private parts: { [id: string]: Part };
+
+    constructor(id: string, prompt: string) {
+        this.id = id;
+        this.prompt = prompt;
+        this.parts = {};
+    }
+
+    public getId(): string {
+        return this.id;
+    }
+
+    public setId(id: string) {
+        this.id = id;
+    }
+
+    public getPrompt(): string {
+        return this.prompt;
+    }
+
+    public setPrompt(prompt: string) {
+        this.prompt = prompt;
+    }
+
+    public getParts(): { readonly [id: string]: Part } {
+        return this.parts;
+    }
+
+    public getPart(id: string): Part {
+        return this.parts[id];
+    }
+
+    public addInput(input: Part) {
+        this.parts[input.getId()] = input;
+    }
+}
